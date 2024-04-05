fb140dc5c75d9e8947e8dbc4b61d72397055f05e
Seth Kunzler <sethkunzler@gmail.com>
2024-04-05 12:01:49 -0600

    first commit

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace keepr_bcw_final.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
