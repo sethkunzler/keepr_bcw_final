69a60bf6aae90922a9d14830461d5923efa83ee5
Seth Kunzler <sethkunzler@gmail.com>
2024-04-05 22:34:28 -0600

    UI: Can create a keep

--- client/src/components/KeepFormModal.vue
+++ client/src/components/KeepFormModal.vue
@@ -8,22 +8,22 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
+        <form @submit.prevent="createKeep">
           <!-- Name description img -->
           <div class="form-floating my-2">
-            <input type="text" class="form-control" id="name" placeholder="Name" required>
+            <input v-model="editableData.name" type="text" class="form-control" id="name" placeholder="Name" minlength="2" maxlength="25" required>
             <label for="name">Name</label>
           </div>
           <div class="form-floating my-2">
-            <input type="url" class="form-control" id="img" placeholder="https://..." required>
+            <input v-model="editableData.img" type="url" class="form-control" id="img" placeholder="https://..." maxlength="1000" required>
             <label for="img">Image Link</label>
           </div>
           <div class="form-floating my-2">
-            <textarea class="form-control" placeholder="Leave a description here" id="description" style="height: 100px" required></textarea>
+            <textarea v-model="editableData.description" class="form-control" placeholder="Write the description here" id="description" style="height: 100px" maxlength="1000" required></textarea>
             <label for="description">Description</label>  
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" required>Close</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" maxlength="500" required>Close</button>
             <button type="submit" class="btn btn-primary">Submit</button>
           </div>
         </form>
@@ -35,10 +35,28 @@
 
 
 <script>
+import { ref } from 'vue'
+import Pop from '../utils/Pop.js'
+import { Modal } from 'bootstrap'
+import { keepsService } from '../services/KeepsService.js'
 export default {
-setup(){
-return{}
-}
+  setup(){
+    const editableData = ref({ name: '', description: '', img: ''})
+    return{
+      editableData,
+      async createKeep() {
+        try {
+          const keep = await keepsService.createKeep(editableData.value)
+          editableData.value = ref({ name: '', description: '', img: ''})
+          Modal.getOrCreateInstance('#keepFormModal').hide()
+          Pop.success(`${keep.name} has been created`)
+        }
+        catch (error){
+          Pop.error(error);
+        }
+      }
+    }
+  }
 }
 </script>
 
--- client/src/services/KeepsService.js
+++ client/src/services/KeepsService.js
@@ -4,7 +4,14 @@ import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class KeepsService {
+  async createKeep(keepData) {
+    const res = await api.post('api/keeps', keepData)
+    logger.log('Created Keep', res.data)
+    const newKeep = new Keep(res.data)
+    AppState.keeps.push(newKeep)
+    return newKeep
 
+  }
   async getKeeps() {
     const res = await api.get("api/keeps")
     logger.log("these are all the keeps returned from the api", res.data)
