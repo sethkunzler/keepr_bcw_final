f02b407e94c65692c28e8b4c636ed9ab634e5b7b
Seth Kunzler <sethkunzler@gmail.com>
2024-04-10 16:26:45 -0600

    Postman: Get My Vaults; (logged in user)

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -5,12 +5,14 @@ namespace keepr_bcw_final.Controllers;
 public class AccountController : ControllerBase
 {
   private readonly AccountService _accountService;
+  private readonly VaultsService _vaultsService;
   private readonly Auth0Provider _auth0Provider;
 
-  public AccountController(AccountService accountService, Auth0Provider auth0Provider)
+  public AccountController(AccountService accountService, Auth0Provider auth0Provider, VaultsService vaultsService)
   {
     _accountService = accountService;
     _auth0Provider = auth0Provider;
+    _vaultsService = vaultsService;
   }
 
   [HttpGet]
@@ -28,10 +30,29 @@ public class AccountController : ControllerBase
     }
   }
 
+  [HttpGet("vaults")]
+  [Authorize]
+  public async Task<ActionResult<List<Vault>>> GetMyVaults()
+  {
+    try
+    {
+      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      string profileId = userInfo.Id; 
+      // The method calls for a profile Id to verify against the current logged in user, but we already did this with [Authorize]
+      // as one lazy developer to another - why reinvent the wheel if a round one will always be better than a square one.
+      // you should always cut corners for the same reason, it get's you places faster.
+      
+      List<Vault> vaults = _vaultsService.GetVaultsByProfileId(userInfo.Id, profileId);
+      return Ok(vaults);
+    }
+    catch (Exception exception)
+    {
+      return BadRequest(exception.Message);
+    }
+  }
+
 
   // TODO edit account
-  // TODO get account keeps
-  // TODO get account vaults
 
 
 
